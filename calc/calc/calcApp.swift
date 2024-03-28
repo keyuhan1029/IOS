5ffcb85b201262186fdef1a71071f86356e1d4d1
keyuhan <keyuhan1029@gmail.com>
2024-03-28 23:38:43 +0800

    rrr

---
+++ calc/calc/calcApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcApp.swift
+//  calc
+//
+//  Created by Keyuhan on 2024/3/28.
+//
+
+import SwiftUI
+
+@main
+struct calcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
